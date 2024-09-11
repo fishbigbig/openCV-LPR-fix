af4870b11eb480f1b35ec5596c6d3c2b894382f6
fishbigbig <39493526@qq.com>
2024-09-11 10:12:10 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# python-opencv(车牌识别)
+
+> 用python3+opencv3做的车牌识别，包括算法和客户端界面
+> 
+> `surface.py`是界面代码
+> 
+> `predict.py`是算法代码(`tkinter`)。
+
+## 使用方法：
+
+>版本：`python3.10.1`，`opencv3.4`和`numpy1.14`和`PIL5`<br>
+
+1. 确保本机已经安装`python3.x`以及`pip`
+
+2. 进入项目根目录，执行`install.sh`来安装对应的依赖
+
+   ```
+   ./install.sh
+   ```
+
+3. 执行成功后可以直接执行`run.sh` 或者 `python ./surface.py`
+
+## 成果展示
+
+![](./Screenshots/分析结果成功1.png)
+
+
+## 算法实现：
+
+1. 先使用图像边缘和车牌颜色定位车牌
+2. 识别字符
+3. 车牌定位在predict方法中
+   1. 车牌字符识别使用的算法是opencv的SVM
+   2. opencv的SVM使用代码来自于opencv附带的sample
+   3. StatModel类和SVM类都是sample中的代码
+4. SVM训练使用的训练样本来自于github上的EasyPR的c++版本。
+5. 由于训练样本有限
+6. 上传了EasyPR中的训练样本，在`train\`目录下
+7. 如果要重新训练请解压在当前目录下，并删除原始训练数据文件`svm.dat`和`svmchinese.dat`。
+
+## 流程概述
+
+### 整体流程
+
+![](./Screenshots/流程图.png)
+
+### 车牌定位流程
+
+![](./Screenshots/车牌定位过程.png)
+
+### 读取车牌字符
+
+![](./Screenshots/读取车牌字符.png)
+# openCV-LPR-fix
---
+++ surface.py
@@ -0,0 +1,189 @@
+import tkinter as tk  
+from tkinter.filedialog import *  
+from tkinter import ttk  
+import predict  
+import cv2  
+from PIL import Image, ImageTk  
+import threading  
+import time  
+
+
+class Surface(ttk.Frame):  
+    pic_path = ""  
+    view_height = 400  
+    view_width = 400  
+    update_time = 0  
+    thread = None  
+    thread_run = False  
+    camera = None  
+    color_transform = {  
+        "green": ("绿牌", "#55FF55"),  
+        "yellow": ("黄牌", "#FFFF00"),  
+        "blue": ("蓝牌", "#6666FF")  
+    }  
+
+    # 初始化  
+    def __init__(self, win):  
+        ttk.Frame.__init__(self, win)  
+        img_area = ttk.Frame(self)  
+        analysis_img_info = ttk.Frame(self)  
+        analysis_res_data = ttk.Frame(self)  
+
+        # 设置Header  
+        header_label = tk.Label(  
+            win,  
+            text='基于OpenCV的SVM算法实现的车牌识别',  
+            bg='#0088FF',  
+            font=('Arial', 16),  
+            fg="#fff", height=2  
+        )  
+        header_label.pack()  
+
+        win.title("OpenCV LPR")  
+        win.state("zoomed")  
+
+        self.pack(fill=tk.BOTH, expand=tk.YES, padx="5", pady="5")  
+        img_area.pack(side=tk.LEFT, expand=1, fill=tk.BOTH)  
+        analysis_img_info.pack(side=tk.TOP, expand=1, fill=tk.Y)  
+        analysis_res_data.pack(side=tk.RIGHT, expand=0)  
+
+        tk.Label(  
+            img_area,  
+            text='需要进行车牌提取的图片：',  
+            font=('Arial', 16),  
+        ).pack(anchor="nw")  
+
+        tk.Label(  
+            analysis_img_info,  
+            text='车牌截取图片：',  
+            font=('Arial', 16),  
+        ).grid(column=0, row=0, sticky=tk.W)  
+
+        from_pic_ctl = tk.Button(  
+            analysis_res_data,  
+            text="选择图片",  
+            width=20,  
+            height=2,  
+            activebackground="#fff",  
+            activeforeground="#0088FF",  
+            font=('Arial', 16),  
+            bg="#fff",  
+            command=self.from_pic  
+        )  
+        self.image_ctl = ttk.Label(img_area)  
+        self.image_ctl.pack(anchor="nw")  
+
+        self.roi_ctl = ttk.Label(analysis_img_info)  
+        self.roi_ctl.grid(column=0, row=1, sticky=tk.W)  
+
+        ttk.Label(  
+            analysis_img_info,  
+            text='识别结果：',  
+            font=('Arial', 16),  
+        ).grid(column=0, row=8, sticky=tk.W)  
+
+        self.r_ctl = ttk.Label(  
+            analysis_img_info,  
+            font=('Arial', 16),  
+            text=""  
+        )  
+        self.r_ctl.grid(column=0, row=12, sticky=tk.W)  
+
+        self.color_ctl = ttk.Label(  
+            analysis_img_info,  
+            text="",  
+            font=('Arial', 16),  
+            width="20",  
+        )  
+        self.color_ctl.grid(column=0, row=16, sticky=tk.W)  
+        from_pic_ctl.pack(anchor="se", pady="5")  
+        self.predictor = predict.CardPredictor()  
+        self.predictor.train_svm()  
+
+    # 图片展示  
+    def get_imgtk(self, img_bgr):  
+        img = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)  
+        im = Image.fromarray(img)  
+        imgtk = ImageTk.PhotoImage(image=im)  
+        wide = imgtk.width()  
+        high = imgtk.height()  
+        if wide > self.view_width or high > self.view_height:  
+            wide_factor = self.view_width / wide  
+            high_factor = self.view_height / high  
+            factor = min(wide_factor, high_factor)  
+
+            wide = int(wide * factor)  
+            if wide <= 0: wide = 1  
+            high = int(high * factor)  
+            if high <= 0: high = 1  
+            im = im.resize((wide, high), Image.LANCZOS)  # 使用 LANCZOS 代替 ANTIALIAS  
+            imgtk = ImageTk.PhotoImage(image=im)  
+        return imgtk  
+
+    # 展示结果  
+    def show_roi(self, r, roi, color):  
+        if r:  
+            roi = cv2.cvtColor(roi, cv2.COLOR_BGR2RGB)  
+            roi = Image.fromarray(roi)  
+            self.imgtk_roi = ImageTk.PhotoImage(image=roi)  
+            self.roi_ctl.configure(image=self.imgtk_roi, state='enable')  
+
+            # 控制车牌号的展示  
+            self.r_ctl.configure(text="".join(r))  
+            self.update_time = time.time()  
+
+            # 控制结果展示以及背景色控制  
+            try:  
+                c = self.color_transform[color]  
+                self.color_ctl.configure(text=c[0], background=c[1], state='enable')  
+            except KeyError:  
+                self.color_ctl.configure(state='disabled')  
+        elif self.update_time + 8 < time.time():  
+            self.roi_ctl.configure(state='disabled')  
+            self.r_ctl.configure(text="")  
+            self.color_ctl.configure(state='disabled')  
+
+    # 选择图片  
+    def from_pic(self):  
+        self.thread_run = False  
+        self.pic_path = askopenfilename(title="选择识别图片", filetypes=[("jpg图片", "*.jpg")])  
+        if self.pic_path:  
+            img_bgr = predict.imreadex(self.pic_path)  
+            self.imgtk = self.get_imgtk(img_bgr)  
+            self.image_ctl.configure(image=self.imgtk)  
+            resize_rates = (1, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4)  
+            for resize_rate in resize_rates:  
+                print("resize_rate:", resize_rate)  
+                r, roi, color = self.predictor.predict(img_bgr, resize_rate)  
+                if r:  
+                    break  
+            self.show_roi(r, roi, color)  
+
+    @staticmethod  
+    def video_thread(self):  
+        self.thread_run = True  
+        predict_time = time.time()  
+        while self.thread_run:  
+            _, img_bgr = self.camera.read()  
+            self.imgtk = self.get_imgtk(img_bgr)  
+            self.image_ctl.configure(image=self.imgtk)  
+            if time.time() - predict_time > 2:  
+                r, roi, color = self.predictor.predict(img_bgr)  
+                self.show_roi(r, roi, color)  
+                predict_time = time.time()  
+        print("run end")  
+
+
+def close_window():  
+    print("destroy")  
+    if surface.thread_run:  
+        surface.thread_run = False  
+        surface.thread.join(2.0)  
+    win.destroy()  
+
+
+if __name__ == '__main__':  
+    win = tk.Tk()  
+    surface = Surface(win)  
+    win.protocol('WM_DELETE_WINDOW', close_window)  
+    win.mainloop()
